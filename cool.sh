006ab9984231115ecc5e40430cb2952a30c3bbb2
trevorbryant <trevorbryant@gmail.com>
2020-02-23 11:14:32 -0500

    cool update

--- cool.sh
+++ cool.sh
@@ -32,12 +32,12 @@ extract_handshake() {
 aircrack_handshake() {
 
     # if not exist, create dir
-    [[ -d aircrack ]] || mkdir aircrack
+    [[ -d aircrack ]] || mkdir cracked
 
     macs=$(cat inventory.csv | cut -d, -f2)
     for mac in $macs
     do
-        aircrack-ng -r /root/wifi/dbs/wctfdb $1 -b $mac -1 -l aircrack/$1.aircracked
+        aircrack-ng -r /root/wifi/dbs/wctfdb $1 -b $mac -1 -l cracked/$1.aircracked # Use -1 for WEP
         grep FOUND $1 | sed -i -e "s,\x1B\[[0-9;]*[a-zA-Z],,g" $1
     done
 
@@ -46,33 +46,36 @@ aircrack_handshake() {
 hashcat_handshake() {
 
    # if not exist, create dir
-   [[ -d hashcat ]] || mkdir hashcat
+   [[ -d hashcat ]] || mkdir cracked
 
-   /usr/bin/hashcat -m 2500 $1 /usr/share/wordlists/cyberpunk.words -o hashcat/$1.hashcat --force 2>/dev/null
+   /usr/bin/hashcat -m 2500 $1 /usr/share/wordlists/cyberpunk.words -o cracked/$1.hashcat --force 2>/dev/null
 
 }
 
+# Create station/AP inventory
 list_files $1
 echo -e "Creating inventory ..."
-
 for file in $files;
 do
     create_inventory $file
 done | sort -u | tee inventory.csv
 sed -i -e s/\"//g inventory.csv
 
+# Extract known good handshakes from all pcaps
 for file in $files;
 do
     echo -e "\nExtracting handshakes for $file ..."
     extract_handshake $file
 done
 
+# Crack handshakes using aircradk-ng
 for file in $(ls *.hccapx);
 do
     echo -e "\naircrack-ng: cracking $file"
     aircrack_handshake $file
 done
 
+# Crack handshakes using hashcat
 for file in $(ls *.hccapx);
 do
     echo -e "\nhashcat: cracking $file"
